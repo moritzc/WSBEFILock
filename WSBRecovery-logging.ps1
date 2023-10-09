357c2ec659c1446157e7e459150164c25fbb0541
moritzc <72256420+moritzc@users.noreply.github.com>
2023-10-09 10:16:29 +0200

    Add files via upload
    Initial upload

---
+++ WSBRecovery-logging.ps1
@@ -0,0 +1,40 @@
+$StartTime = (Get-Date).AddHours(-12)
+$EndTime = Get-Date
+$LogName = 'Microsoft-Windows-Backup'
+$EventID = 5
+$Keyword = '0x8078011E'
+$LogFile = "C:\Temp\WSBRecoverylog.txt"
+
+Function WriteLog ($Message) {
+    $Timestamp = (Get-Date).ToString("dd-MM-yyyy HH:mm:ss")
+    $LogMessage = "$Timestamp - $Message"
+    Add-Content -Path $LogFile -Value $LogMessage
+}
+
+$Events = Get-WinEvent -FilterHashtable @{
+    LogName = $LogName
+    ID = $EventID
+    StartTime = $StartTime
+    EndTime = $EndTime
+} -ErrorAction SilentlyContinue | Where-Object { $_.Message -match $Keyword }
+
+If ($Events.Count -ge 1) {
+    $Message = "Backup failed with EFI lock error"
+    WriteLog $Message
+    
+    $CurrentBackupJob = Get-WBJob -ErrorAction SilentlyContinue
+
+    If ($null -eq $CurrentBackupJob) {
+        Start-WBBackup -Policy Get-WBPolicy -Async
+        $Message = "No backup was running. A new backup has been started."
+        WriteLog $Message
+    }     
+    Else {
+        $Message = "A backup is already running. No action taken."
+        WriteLog $Message
+    }
+}
+Else {
+    $Message = "No EFI lock error. Check for other issues!"
+    WriteLog $Message
+}
---
+++ WSBRecovery.ps1
@@ -0,0 +1,27 @@
+$StartTime = (Get-Date).AddHours(-12)
+$EndTime = Get-Date
+$LogName = 'Microsoft-Windows-Backup'
+$EventID = 5
+$Keyword = '0x8078011E'
+
+$Events = Get-WinEvent -FilterHashtable @{
+    LogName = $LogName
+    ID = $EventID
+    StartTime = $StartTime
+    EndTime = $EndTime
+} -ErrorAction SilentlyContinue | Where-Object { $_.Message -match $Keyword }
+
+If ($Events.Count -ge 1) {
+    Write-Output "Backup failed with EFI lock error"
+	$CurrentBackupJob = Get-WBJob -ErrorAction SilentlyContinue
+
+	If ($null -eq $CurrentBackupJob) {
+		Start-WBBackup -Policy Get-WBPolicy -Async
+		Write-Output "No backup was running. A new backup has been started."
+	} 	
+	Else {
+		Write-Output "A backup is already running. No action taken."
+	}
+}
+
+Else {Write-Output "No EFI lock error. Check for other issues!"}
